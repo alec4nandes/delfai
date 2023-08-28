0497625fd8b0edc68f345fde96dcb80adc8b43ef
Alec <alec4nandes@gmail.com>
2023-08-28 14:26:37 -0700

    create stream api

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
-const express = require("express"),
+const OpenAI = require("openai"),
+    { apiKey } = require("./api-key.js"),
+    express = require("express"),
     bodyParser = require("body-parser"),
     cors = require("cors"),
-    { getSpreadData } = require("./read-cards.js"),
     functions = require("firebase-functions");
 
-const server = express();
+const openai = new OpenAI({ apiKey }),
+    server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
@@ -14,10 +16,51 @@ server.use(
     })
 );
 
-server.post("/", async function (req, res) {
-    const { question } = req.body;
-    res.send(await getSpreadData(question));
-});
+["past", "present", "future", "advice"].forEach(createRoute);
+
+function createRoute(timeframe) {
+    const isAdvice = timeframe === "advice";
+    server.post(`/${timeframe}`, async function (req, res) {
+        const { card, cards, question } = req.body,
+            content = isAdvice
+                ? getAdviceContent(cards, question)
+                : getContent(card, timeframe, question),
+            stream = await getStream(content);
+        for await (const part of stream) {
+            const moreText = part.choices[0]?.delta?.content || "";
+            res.write(moreText);
+        }
+        res.end();
+    });
+}
+
+function getAdviceContent(cards, question) {
+    return `What life advice would you give me if I drew the following tarot cards: ${cards
+        .map(
+            (card, i) =>
+                `${card} for my ${i ? (i > 1 ? "future" : "present") : "past"}`
+        )
+        .join(", ")
+        .trim()}. Please write only one paragraph. ${askQuestion(question)}`;
+}
+
+function getContent(card, timeframe, question) {
+    return `Please write only one paragraph about the tarot card ${card} and how it can relate to my ${timeframe}. ${askQuestion(
+        question
+    )}`;
+}
+
+function askQuestion(question) {
+    return `If, and only if, my question: "${question}" makes sense, please address it in your paragraph.`;
+}
+
+async function getStream(content) {
+    return await openai.chat.completions.create({
+        messages: [{ role: "user", content }],
+        model: "gpt-3.5-turbo",
+        stream: true,
+    });
+}
 
 const api = functions.https.onRequest(server);
 
--- public/display.js
+++ public/display.js
@@ -1,53 +1,62 @@
-async function displaySpread(data) {
-    const spreadElem = document.querySelector("#spread"),
-        loadingElem = document.querySelector("#loading"),
-        questionElem = document.querySelector("#question"),
-        { question } = data;
-    delete data.question;
-    Object.entries(data).forEach(([key, info]) => {
-        const elem = spreadElem.querySelector(`#${key}`);
-        loadReading(elem, info);
-    });
-    loadingElem.style.display = "none";
-    spreadElem.style.display = "flex";
-    questionElem.innerHTML = question;
-}
-
-function loadReading(elem, info) {
-    const { card, reading } = info;
-    if (card) {
-        elem.querySelector(".card").textContent = card;
-        const imgElem = elem.querySelector("img");
-        imgElem.src = `/assets/cards/${card.replace(" reversed", "")}.jpg`;
-        imgElem.alt = card;
-        card.includes(" reversed") && imgElem.classList.add("reversed");
-    }
-    elem.querySelector(".reading").textContent = reading;
-}
+import getRandomCards from "./deck.js";
 
 async function handleSubmit(e) {
     e.preventDefault();
-    const loadingElem = document.querySelector("#loading");
+    const cards = getRandomCards(),
+        spreadElem = document.querySelector("#spread");
     e.target.style.display = "none";
-    loadingElem.style.display = "block";
+    spreadElem.style.display = "flex";
+    ["past", "present", "future", "advice"].forEach((timeframe, i) =>
+        displayData(timeframe, i, cards, spreadElem)
+    );
+}
+
+async function displayData(timeframe, i, cards, spreadElem) {
     try {
-        const // endpoint = "http://localhost:5001/delfai/us-central1/api",
-            endpoint = "https://us-central1-delfai.cloudfunctions.net/api",
+        const endpoint = "http://localhost:5001/delfai/us-central1/api",
+            // endpoint = "https://us-central1-delfai.cloudfunctions.net/api",
+            card = cards[i],
             question = e.target.question.value,
-            response = await fetch(endpoint, {
+            response = await fetch(`${endpoint}/${timeframe}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ question }),
-            }),
-            data = await response.json();
-        displaySpread(data);
+                body: JSON.stringify({ card, cards, question }),
+            });
+        const timeElem = spreadElem.querySelector(`#${timeframe}`),
+            cardElem = timeElem.querySelector(".card"),
+            imgElem = timeElem.querySelector("img"),
+            readingElem = timeElem.querySelector(".reading");
+        cardElem && (cardElem.textContent = card);
+        if (imgElem) {
+            imgElem.src = `/assets/cards/${card.replace(" reversed", "")}.jpg`;
+            imgElem.alt = card;
+            card.includes(" reversed") && imgElem.classList.add("reversed");
+        }
+        fetchStream(response.body, timeframe, readingElem);
     } catch (err) {
-        loadingElem.textContent = err.message;
-        loadingElem.classList.add("error");
+        spreadElem.innerHTML = `${err.code}: ${err.message}`;
+        spreadElem.classList.add("error");
     }
 }
 
-export default displaySpread;
+function fetchStream(stream, timeframe, readingElem) {
+    const reader = stream.getReader();
+    // read() returns a promise that fulfills
+    // when a value has been received
+    reader.read().then(function processText({ done, value }) {
+        // Result objects contain two properties:
+        // done  - true if the stream has already given you all its data.
+        // value - some data. Always undefined when done is true.
+        if (done) {
+            console.log(`${timeframe} stream complete!`);
+            return;
+        }
+        readingElem.textContent += new TextDecoder().decode(value);
+        // Read some more, and call this function again
+        return reader.read().then(processText);
+    });
+}
+
 export { handleSubmit };
