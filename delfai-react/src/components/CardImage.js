a93cc0b1e9b6e65c69fc24a69a1e461e19e4454f
Alec <alec4nandes@gmail.com>
2023-08-29 13:21:52 -0700

    rotate reversed cards

--- delfai-react/src/components/CardImage.js
+++ delfai-react/src/components/CardImage.js
@@ -1,7 +1,9 @@
 export default function CardImage({ card }) {
     return (
         <img
-            className="card-image"
+            className={`card-image ${
+                card.includes(" reversed") ? "reversed" : ""
+            }`}
             src={`/assets/cards/${card.replace(" reversed", "")}.jpg`}
             alt={card}
         />
