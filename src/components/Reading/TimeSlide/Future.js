024cb77fd343b2f7f40b1a8fbb80430312f41e3d
Alec <alec4nandes@gmail.com>
2023-09-02 15:11:13 -0700

    style payment form

--- src/components/Reading/TimeSlide/Future.js
+++
@@ -1,9 +0,0 @@
-import TimeSlide from "./TimeSlide";
-
-export default function Future({ card, question, elemRef, waitRef }) {
-    return (
-        <TimeSlide
-            {...{ card, timeframe: "Future", question, elemRef, waitRef }}
-        />
-    );
-}
--- src/components/Reading/TimeSlide/Present.js
+++
@@ -1,9 +0,0 @@
-import TimeSlide from "./TimeSlide";
-
-export default function Present({ card, question, elemRef, waitRef }) {
-    return (
-        <TimeSlide
-            {...{ card, timeframe: "Present", question, elemRef, waitRef }}
-        />
-    );
-}
