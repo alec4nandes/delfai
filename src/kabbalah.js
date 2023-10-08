2ad2f35b1a43371704a1cd6ee8b04bc26bd3441c
Alec <alec4nandes@gmail.com>
2023-10-07 22:28:16 -0700

    add kabbalah data

---
+++ src/kabbalah.js
@@ -0,0 +1,138 @@
+const kabbalah = {
+    Fool: {
+        letter: "א",
+        element: "Air",
+    },
+    Magician: {
+        letter: "ב",
+        astro: "Mercury",
+    },
+    "High Priestess": {
+        letter: "ג",
+        astro: "Moon",
+    },
+    Empress: {
+        letter: "ד",
+        astro: "Venus",
+    },
+    Emperor: {
+        letter: "ה",
+        zodiac: {
+            sign: "Aries",
+            start_date: "3/21",
+            end_date: "4/20",
+        },
+    },
+    Hierophant: {
+        letter: "ו",
+        zodiac: {
+            sign: "Taurus",
+            start_date: "4/21",
+            end_date: "5/20",
+        },
+    },
+    Lovers: {
+        letter: "ז",
+        zodiac: {
+            sign: "Gemini",
+            start_date: "5/21",
+            end_date: "6/20",
+        },
+    },
+    Chariot: {
+        letter: "ח",
+        zodiac: {
+            sign: "Cancer",
+            start_date: "6/21",
+            end_date: "7/21",
+        },
+    },
+    Strength: {
+        letter: "ט",
+        zodiac: {
+            sign: "Leo",
+            start_date: "7/22",
+            end_date: "8/22",
+        },
+    },
+    Hermit: {
+        letter: "י",
+        zodiac: {
+            sign: "Virgo",
+            start_date: "8/23",
+            end_date: "9/22",
+        },
+    },
+    "Wheel of Fortune": {
+        letter: "כ",
+        astro: "Jupiter",
+    },
+    Justice: {
+        letter: "ל",
+        zodiac: {
+            sign: "Libra",
+            start_date: "9/23",
+            end_date: "10/22",
+        },
+    },
+    "Hanged Man": {
+        letter: "מ",
+        element: "Water",
+    },
+    Death: {
+        letter: "נ",
+        zodiac: {
+            sign: "Scorpio",
+            start_date: "10/23",
+            end_date: "11/22",
+        },
+    },
+    Temperance: {
+        letter: "ס",
+        zodiac: {
+            sign: "Sagittarius",
+            start_date: "11/23",
+            end_date: "12/21",
+        },
+    },
+    Devil: {
+        letter: "ע",
+        zodiac: {
+            sign: "Capricorn",
+            start_date: "12/22",
+            end_date: "1/19",
+        },
+    },
+    Tower: {
+        letter: "פ",
+        astro: "Mars",
+    },
+    Star: {
+        letter: "צ",
+        zodiac: {
+            sign: "Aquarius",
+            start_date: "1/20",
+            end_date: "2/18",
+        },
+    },
+    Moon: {
+        letter: "ק",
+        zodiac: {
+            sign: "Pisces",
+            start_date: "2/19",
+            end_date: "3/20",
+        },
+    },
+    Sun: {
+        letter: "ר",
+        astro: "Sun",
+    },
+    Judgement: {
+        letter: "ש",
+        element: "Fire",
+    },
+    World: {
+        letter: "ת",
+        astro: "Saturn",
+    },
+};
