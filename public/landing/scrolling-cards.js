250dadca4a647be5a95b96d0dae8a29ff3f4e01b
Alec <alec4nandes@gmail.com>
2023-10-16 12:55:07 -0700

    build landing page

---
+++ public/landing/scrolling-cards.js
@@ -0,0 +1,21 @@
+const headerElem = document.querySelector("header"),
+    scrollCardsElems = [...headerElem.querySelectorAll(".scroll-cards")],
+    cardNames = ["Fool", "High Priestess", "Wheel of Fortune", "Star", "World"],
+    getCardImgs = (isReversed) =>
+        (isReversed ? [...cardNames].reverse() : cardNames)
+            .map((cardName) => `<img src="/assets/cards/${cardName}.jpg"/>`)
+            .join(""),
+    addCardImgs = (elem, i) => (elem.innerHTML += getCardImgs(!!i));
+
+scrollCardsElems.forEach((elem, i) => {
+    addCardImgs(elem, i);
+    setInterval(() => {
+        const oldTop = elem.scrollTop;
+        elem.scrollTop += 1;
+        if (elem.scrollTop === oldTop) {
+            // reached end of scroll
+            console.log("adding cards images...");
+            addCardImgs(elem, i);
+        }
+    }, i * 5 + 25);
+});
