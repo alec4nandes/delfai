977d54402d08105beebe3ac3912befd0a1257ccf
Alec <alec4nandes@gmail.com>
2023-10-20 12:20:08 -0700

    add age and verification screens

---
+++ deploy/scripts/verify-age-location.js
@@ -0,0 +1,30 @@
+const cookieKey = "age_location_verified",
+    hideVerifyScreen = () =>
+        (document.querySelector("#verify-age-location").style.display = "none");
+
+// handle Enter
+document.querySelector("#enter-btn").onclick = () => {
+    document.cookie = `${cookieKey}=1`;
+    hideVerifyScreen();
+};
+
+// check cookie
+const cookieData = parseCookie(document.cookie),
+    userVerified = !!+cookieData[cookieKey];
+console.log("user verified:", userVerified);
+userVerified && hideVerifyScreen();
+
+// https://www.geekstrick.com/snippets/how-to-parse-cookies-in-javascript/
+// cookie in browser: `pkg=math; equation=E%3Dmc%5E2`
+// parsed: { pkg: 'math', equation: 'E=mc^2' }
+function parseCookie(str) {
+    return str
+        .split(";")
+        .map((v) => v.split("="))
+        .reduce((acc, v) => {
+            acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(
+                v[1].trim()
+            );
+            return acc;
+        }, {});
+}
